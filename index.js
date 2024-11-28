e1b3e88bb8b1e8074a2ede782ed9a50cbcb607c6
juliamistieri <julia.mistieri02@gmail.com>
2024-11-28 16:02:38 -0400

    Revert "Merge branch 'main' of https://github.com/juliamistieri/projectPTAS3"
    This reverts commit 0b21341d29eacc387241237257e6feab07fafcef, reversing
    changes made to 42781e990df6a470f2d355787ae40cecc1975a0d.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const prisma = require("./prisma/prismaClient");
-const cors = require('cors')
 
 const express = require("express")
 
 const app =  express();
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true}));
