efb03c5437e4bcd42990958e436cdcf01e5d2466
juliamistieri <julia.mistieri02@gmail.com>
2024-11-28 16:14:55 -0400

    kkk

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const prisma = require("./prisma/prismaClient");
+const cors = require('cors')
 
 const express = require("express")
 
 const app =  express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true}));
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
-
-app.listen(8000);
